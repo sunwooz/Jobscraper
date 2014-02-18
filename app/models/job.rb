0370696cc063f0f1dc0b5dc15e8b88d4f7242965
Daniel Yang <yangsunwoo@gmail.com>
2014-02-18 17:28:31 -0500

    changed hover state of accordion

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,16 +2,7 @@ require 'nokogiri'
 require 'open-uri'
 
 class Job < ActiveRecord::Base
-	#   t.text     "content"
-	#   t.timestamp "created_at"
-	#   t.timestamp "updated_at"
-
 	validates :content, uniqueness: true
-
-	# searchable do
-	# 	text :content
-	# 	time :created_at
-	# end
 
 	def self.search(terms = "")
 		if terms.blank?
