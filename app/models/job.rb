6c887f0f5452a3f8cb31a20a845311eab83eb33a
Daniel Yang <yangsunwoo@gmail.com>
2014-02-22 00:48:03 -0500

    Finished location based searching with sanitize bugs

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,37 +1,9 @@
-require 'nokogiri'
-require 'open-uri'
-
 class Job < ActiveRecord::Base
 	validates :content, uniqueness: true
 
-	def self.search(terms = "", location)
-		# if terms.blank?
-		# 	result = Job.all(limit: 15)
-		# else
-		# 	sanitized = sanitize_sql_array(["to_tsquery('english', ?)", terms.gsub(/\s/,"+")])
-		# 	result = Job.where("search_vector @@ #{sanitized}")
-		# end
-		# result
-
-		if terms.blank?
-			if location == "All Cities"
-				sanitized = sanitize_sql_array(["to_tsquery('english', ?)", terms.gsub(/\s/,"+")])
-				result = Job.all
-			elsif !location.blank?
-				sanitized_location_query = Job.to_location_query(location)
-				result = Job.where("search_vector @@ #{sanitized_location_query}")
-			else
-				result = Job.all(limit:15)
-			end
-
-		elsif !terms.blank?
-			if location == "All Cities"
-				sanitized = sanitize_sql_array(["to_tsquery('english', ?)", terms.gsub(/\s/, "+")])
-				result = Job.where("search_vector @@ #{sanitized}")
-			else #elsif location is something different
-				#filter for the location + all query results
-			end
-		end
+	def self.search(terms="", location)
+		result = Job.search_result_for_blank_query(terms, location) if terms.blank?
+		result = Job.search_result_for_query(terms, location) if !terms.blank?
 		result
 	end
 
@@ -46,5 +18,36 @@ class Job < ActiveRecord::Base
 			end.join("|")
 			location_query = "(" + location_query + ")"
 			sanitized_locations = sanitize_sql_array(["to_tsquery('english', ?)", location_query])
+		end
+
+		def self.search_result_for_query(terms, location)
+			if location == "All Cities"
+				sanitized = sanitize_sql_array(["to_tsquery('english', ?)", terms.gsub(/\s/, "+")])
+				result = Job.where("search_vector @@ #{sanitized}")
+			else location != "All Cities"
+				cities = {
+					'New York City' => ['NYC', 'new york city', 'new york', 'ny'],
+					'San Francisco' => ['SF', 'San', 'SFrancisco']
+				}
+				location_query = cities[location].map do |city|
+						city.gsub(/\s/, "+")
+				end.join("|")
+				location_query = "(" + location_query + ")"
+				result = Job.where("search_vector @@ to_tsquery('english', '#{location_query} & #{terms.gsub(/\s/, '+')}')")
+			end
+			result
+		end
+
+		def self.search_result_for_blank_query(terms, location)
+			if location == "All Cities"
+				sanitized = sanitize_sql_array(["to_tsquery('english', ?)", terms.gsub(/\s/,"+")])
+				result = Job.all
+			elsif !location.blank?
+				sanitized_location_query = Job.to_location_query(location)
+				result = Job.where("search_vector @@ #{sanitized_location_query}")
+			else
+				result = Job.all(limit:15)
+			end
+			result
 		end
 end
